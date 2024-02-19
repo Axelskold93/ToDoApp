72c96bfad5e700afe6e8b3c6803e505f3053e4f3
Axelskold93 <142592414+Axelskold93@users.noreply.github.com>
2024-02-19 14:38:33 +0100

    Added functions to delete items and added css and checkbox to list items

--- main.js
+++ main.js
@@ -1,15 +1,56 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let todoList = docment.querySelector('.todo-list');
-    let newtoDo = document.querySelector('#input-bar');
+    document.addEventListener("DOMContentLoaded", function() {
+    let todoList = document.querySelector('.todo-list');
+    let inputBar = document.querySelector('#input-bar');
     let toggleButton = document.querySelector('#toggle');
+    toggleButton.style.visibility = 'hidden';
     let filters = document.querySelectorAll('.filters a');
     let todoCount = document.querySelector('.todo-count');
-    let clearCompletedButton = document.querySelector('.clear-completed');
-
+    let clearCompletedButton = document.querySelector('#clear-completed');
+    //eventlistener for input bar
+    inputBar.addEventListener('keydown', function(event){
+        //so that the code only runs when enter is pressed
+       if (event.key === 'Enter') {
+        addListItem(inputBar, todoList);
+        event.preventDefault();
+        //show togglebutton
+        toggleButton.style.visibility = 'visible';
+       }
+    });
     clearCompletedButton.addEventListener('click', function() {
-        const completedTodos = document.querySelectorAll('.todo-item.completed');
-        completedTodos.forEach(todoItem => todoList.removeChild(todoItem));
+        let completedTodos = findCheckedItems();
+        completedTodos.forEach(listItem => todoList.removeChild(listItem));
 
         
     });
 });
+
+function addListItem(inputBar, todoList) {
+    let newItem = inputBar.value;
+    if (newItem !== '') {
+        //create li element
+        let listItem = document.createElement('li')
+        listItem.id = 'list-item';
+        //create checkbox
+        let checkBox = document.createElement('input');
+        checkBox.setAttribute('type', 'checkbox');
+        checkBox.className = 'todo-checkbox';
+        //append items to ul
+        listItem.appendChild(checkBox);
+        listItem.appendChild(document.createTextNode(newItem));
+        todoList.appendChild(listItem);
+        //reset the input bar
+        inputBar.value = '';
+
+    }
+}
+function findCheckedItems() {
+    let checkBoxes = document.getElementsByClassName('todo-checkbox');
+    let completedItems = [];
+    for (let i = 0; i < checkBoxes.length; i++) {
+        if (checkBoxes[i].checked) {
+            completedItems.push(checkBoxes[i].parentNode);
+        }
+    }
+    console.log('Found checkboxes: ', completedItems);
+    return completedItems;
+}
